ea342a0620dadd957cd62c13f6bfcb5911806ff9
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2024-10-27 21:30:35 +0800

    basic env setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+use std::error::Error;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -16,13 +18,22 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // load env even if it fails
+    let _ = dotenvy::dotenv();
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((
+        env::var("HOST").unwrap_or("127.0.0.1".to_string()),
+        env::var("PORT")
+            .unwrap_or("8080".to_string())
+            .parse::<u16>()
+            .expect("PORT must be a number"),
+    ))?
     .run()
     .await
 }
